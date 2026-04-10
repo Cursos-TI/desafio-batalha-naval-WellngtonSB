40993c83be90b98357833bb6bd1fe413edf9bca2
WellngtonSB <wellington.souzask8@gmail.com>
2026-04-10 22:59:04 +0000

    adição de cabeçcalho

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 
-//char linha [0] = {'A','B','C','D','E','F','G','H','I','J',};
+
 
 int main() {
+    //char linha [0] = {'A','B','C','D','E','F','G','H','I','J'};
     int  matrix [10][10] = {0};
-    int i, j, k;
+    int i, j, k, l, n;
     int navioHorizontal[3]= {3,3,3};
     int navioVertical [3] = {3,3,3};
     
+    
     for (k = 0; k < 3; k++){
-        matrix [2][3+k] = navioHorizontal[k];
-        matrix [3+k][6] = navioVertical [k];
-    }
-
-
-    for (i = 0; i < 10; i++){
-        for (j = 0; j < 10; j++){
-            printf("%d ", matrix[i][j]);
+        matrix [2][3+k] = navioHorizontal[k]; // Cria os navio na horizontal
+        matrix [3+k][6] = navioVertical [k];  // Cria os navios na vertical
         
-          }
-            printf("\n");
+    }
+            printf(" # ");
+        for (l ='A'; l <='J'; l++){ // cria indices de letras 
+            printf("%c ", l); 
         }
+        printf("\n");
+    for (i = 0; i < 10; i++){ // loop para Matriz 10x10
+        printf("%2d ", i+1);
+        for (j = 0; j < 10; j++){
+            printf("%d ", matrix[i][j]); // imprime a matriz 10x10 nosso mar
+           
+        }
+            printf("\n");
+            
+       }
         return 0;
     }
     
